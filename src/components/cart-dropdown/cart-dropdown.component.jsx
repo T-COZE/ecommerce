82f982ffa5ddc965ab80b83cb7916b4c800ac388
Thomas Styron <thomasstyron@gmail.com>
2023-10-26 17:58:32 -0500

    cart icon and drop down first draft

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { useContext } from 'react';
 import CartItem from '../cart-item/cart-item.component';
-
+import { CartContext } from '../../contexts/cart.context';
 import Button from '../button/button.component'
 import './cart-dropdown.styles.scss'
 
 const CartDropdown = () =>{
+    const { cartItems } = useContext(CartContext)
     return (
         <div className="cart-dropdown-container">
             <div className='cart-items'>
-                {[].map(item => <CartItem cartItem={item} />)}
+                {cartItems.map((item) => (
+                     <CartItem key={item.id} cartItem={item} />
+                     ))}
             </div>
             <Button>GO TO CHECKOUT</Button>
         </div>
---
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -0,0 +1,9 @@
+import './checkout-item.styles.scss';
+
+const CheckoutItem = () => {
+    return (
+        <div>
+            <div className='checkout-item'>{product.name}</div>
+        </div>
+    )
+}
