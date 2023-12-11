ce7bb5cf0b5b9f0a837248235958956e12224e81
Thomas Styron <thomasstyron@gmail.com>
2023-12-11 08:55:14 -0600

    end of sagas writing

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,42 +1,45 @@
-import { Fragment } from "react";
-import { Outlet } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { ReactComponent as CrownLogo } from "../../assets/crown.svg";
-import CartIcon from "../../components/cart-icon/cart-icon.component";
-import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
-import { selectCurrentUser } from "../../store/user/user.selector";
+import { Fragment } from 'react';
+import { Outlet } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 
-import { selectIsCartOpen } from "../../store/cart/cart.selector"
-import { signOutUser } from "../../utils/firebase/firebase.utils";
-import {NavigationContainer, LogoContainer, NavLinks, NavLink} from "./navigation.styles";
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+
+import { selectIsCartOpen } from '../../store/cart/cart.selector';
+import { selectCurrentUser } from '../../store/user/user.selector';
+import { signOutStart } from '../../store/user/user.action';
+
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+
+import {
+  NavigationContainer,
+  NavLinks,
+  NavLink,
+  LogoContainer,
+} from './navigation.styles';
 
 const Navigation = () => {
-  const currentUser = useSelector(selectCurrentUser)
-  
-  const isCartOpen = useSelector(selectIsCartOpen)
- 
+  const dispatch = useDispatch();
+  const currentUser = useSelector(selectCurrentUser);
+  const isCartOpen = useSelector(selectIsCartOpen);
+
+  const signOutUser = () => dispatch(signOutStart());
 
   return (
     <Fragment>
       <NavigationContainer>
-        <LogoContainer to="/">
-          <CrownLogo className="logo" />
+        <LogoContainer to='/'>
+          <CrwnLogo className='logo' />
         </LogoContainer>
         <NavLinks>
-          <NavLink  to="/shop">
-            SHOP
-          </NavLink>
-          <NavLink to="/contact">
-            CONTACT
-          </NavLink>
+          <NavLink to='/shop'>SHOP</NavLink>
+
           {currentUser ? (
             <NavLink as='span' onClick={signOutUser}>
               SIGN OUT
             </NavLink>
           ) : (
-            <NavLink to="/auth">
-              SIGN IN
-            </NavLink>
+            <NavLink to='/auth'>SIGN IN</NavLink>
           )}
           <CartIcon />
         </NavLinks>
